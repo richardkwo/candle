b69d52596734ac63cac1dbd3064809cafcdfc4b6
wong2 <wonderfuly@gmail.com>
2013-09-29 15:57:31 +0800

    move send to file

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ from send_email import send_file_via_email
 logging.basicConfig(filename='log/app.log', level=logging.DEBUG,
     format='%(asctime)-15s %(name)s.py <%(funcName)s> %(message)s')
 
-with open('data/e2432/data.txt') as fp:
-    data = fp.read()
 
-book_title, encrypted_content = data.split(':')[:2]
-book_content_data = json.loads(decrypt(encrypted_content))
-try:
-    book_mobi_path = generate_book('e2432', book_title, book_content_data)
-except RuntimeError as e:
-    book_mobi_path = None
+def send_book_to_kindle(book_id, to_email):
+    with open('data/%s/data.txt' % book_id) as fp:
+        data = fp.read()
 
-send_file_via_email('wonderfuly@kindle.cn', book_mobi_path)
+    book_title, encrypted_content = data.split(':')[:2]
+    book_content_data = json.loads(decrypt(encrypted_content))
+    try:
+        book_mobi_path = generate_book(book_id, book_title, book_content_data)
+    except RuntimeError as e:
+        book_mobi_path = None
+
+    send_file_via_email(to_email, book_mobi_path)
+
+
+if __name__ == '__main__':
+    send_book_to_kindle('e2432', 'wonderfuly@kindle.cn')
