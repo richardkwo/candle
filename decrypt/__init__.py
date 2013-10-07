36a85590927d8853962d183c5f315f8466a25528
wong2 <wonderfuly@gmail.com>
2013-10-07 20:02:47 +0800

    remove linux decrypt to 64

--- decrypt/__init__.py
+++ decrypt/__init__.py
@@ -1,10 +1,15 @@
 #-*-coding:utf-8-*-
 
+import sys
 import platform
 
 system = platform.system()
+is_64 = sys.maxsize > 2**32
 
 if system == 'Darwin':
     from decrypt_mac import decrypt
 elif system == 'Linux':
-    from decrypt_linux import decrypt
+    if is_64:
+        from decrypt_linux_64 import decrypt
+    else:
+        from decrypt_linux_32 import decrypt
