f61637528877f4ab16a78a6db2a897cd0cb7fc7a
wong2 <wonderfuly@gmail.com>
2013-10-08 22:11:14 +0800

    option page limit to kindle email

--- chrome/scripts/options.js
+++ chrome/scripts/options.js
@@ -24,11 +24,20 @@ $(function() {
   $('.mainview > *:not(.selected)').css('display', 'none');
 
   $('#server').val(localStorage.server);
-  $('#to_email').val(localStorage.to_email);
+
+  var to_email = localStorage.to_email;
+  if (to_email) {
+      var splitter = to_email.split('@');
+      if (splitter.length > 1 ) {
+        $('#kindle-email-name').val(splitter[0]);
+        $('#kindle-email-domain').val(splitter[1]);
+      }
+  }
 
   $('#save-btn').click(function(){
     localStorage.server = $('#server').val();
-    localStorage.to_email = $('#to_email').val();
+    localStorage.to_email = $('#kindle-email-name').val() + 
+        '@' + $('#kindle-email-domain').val();
     $('#saved-tip').fadeIn(500).fadeOut(500);
   });
 });
